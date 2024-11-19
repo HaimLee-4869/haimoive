9ffae894254d763cdae93b3768d7be50fe96bfb7
HaimLee <j_haim4869@naver.com>
2024-11-20 02:31:13 +0900

    start

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode> //잠깐 꺼둠. 새로고침시 렌더링 두번. 
     <App />
-  </React.StrictMode>
+  // </React.StrictMode> //잠깐 꺼둠. 새로고침시 렌더링 두번. 
 );
 
 // If you want to start measuring performance in your app, pass a function
