d853c97d47adc42ee98ff73c43be1a39b2dee570
HaimLee <j_haim4869@naver.com>
2024-11-26 02:14:41 +0900

    API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Popular from "./pages/Popular";
-import Search from "./pages/Search";
-import Wishlist from "./pages/Wishlist";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import Home from './components/Home';
+import Popular from './components/Popular';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Wishlist from './components/Wishlist';
+import Search from './components/Search';
+import './App.css';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  const isLoggedIn = !!(localStorage.getItem('authToken') || sessionStorage.getItem('authToken'));
+
+  if (!isLoggedIn && location.pathname !== '/login' && location.pathname !== '/signup') {
+    // 로그인되지 않은 상태에서 SignIn 또는 SignUp이 아닌 페이지 접근 시 이동
+    return <Navigate to="/signin" replace />;
+  }
+
+  return (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Routes location={location}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          {/* 기본적으로 없는 경로에 대한 처리 */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+}
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/popular" element={<Popular />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-        </Routes>
-      </div>
+    <Router basename="/haimoive">
+      <AnimatedRoutes />
     </Router>
   );
 }
--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,12 +1,20 @@
-import axios from "axios";
+// src/services/tmdbApi.js
+import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3";
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const fetchMovies = async (endpoint, params = {}) => {
-  const url = `${BASE_URL}/${endpoint}`;
-  const response = await axios.get(url, {
-    params: { api_key: API_KEY, ...params },
-  });
-  return response.data;
+  try {
+    const response = await axios.get(`${BASE_URL}${endpoint}`, {
+      params: { api_key: API_KEY, ...params },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data from TMDB:', error);
+    throw error;
+  }
 };
+
+// 사용 예시
+// fetchMovies('/movie/popular', { page: 1 });
